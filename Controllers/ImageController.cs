d672ac17d174afe9109014aa7ebe631a263aa6d0
Zeshan <muhammadzeshawn.1@gmail.com>
2023-03-09 18:05:53 +0500

    delete

--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net.Mime;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using XenonHealth.Task.Models;
 using AppContext = System.AppContext;
 
@@ -24,6 +25,7 @@ namespace XenonHealth.Task.Controllers
         {
             return View();
         }
+
         [Route("{controller}/{id}")]
         public IActionResult ViewImage([FromRoute] string id)
         {
@@ -33,14 +35,17 @@ namespace XenonHealth.Task.Controllers
 
                 return image == null ? NotFound("Image doesn't exist") : View("ViewImage", image);
             }
+
             return BadRequest("The Guid is Invalid");
         }
+
         [Route("{controller}/List")]
         public IActionResult List()
         {
             var imagelist = _dbContext.Images.ToList();
             return View(imagelist);
         }
+
         [HttpPost]
         [RequestSizeLimit(10000000)]
         [Route("{controller}/Create")]
@@ -50,22 +55,25 @@ namespace XenonHealth.Task.Controllers
             {
                 ImageModel domainmodel = new ImageModel();
 
-                if (dtomodel.ImageFile is { Length: > 0 })
+                if (dtomodel.ImageFile is {Length: > 0})
                 {
                     using var memoryStream = new MemoryStream();
                     await dtomodel.ImageFile.CopyToAsync(memoryStream);
                     domainmodel.ImageData = memoryStream.ToArray();
                 }
+
                 domainmodel.ImageName = dtomodel.ImageName;
                 domainmodel.Id = Guid.NewGuid().ToString();
                 domainmodel.ImageExtension = Path.GetExtension(dtomodel.ImageFile.FileName);
-                domainmodel.CreateDate = DateTime.Now; ;
+                domainmodel.CreateDate = DateTime.Now;
+                ;
                 _dbContext.Images.Add(domainmodel);
                 await _dbContext.SaveChangesAsync();
                 TempData["issaved"] = true;
                 TempData["ID"] = domainmodel.Id;
                 return RedirectToAction("Index", "Image");
             }
+
             TempData["issaved"] = false;
             string messages = string.Join("; ", ModelState.Values
                 .SelectMany(x => x.Errors)
@@ -73,6 +81,12 @@ namespace XenonHealth.Task.Controllers
 
             TempData["message"] = messages;
             return View("Index");
+        }
+        [Route("{controller}/Delete")]
+        public IActionResult Delete()
+        {
+            _dbContext.Database.ExecuteSqlRaw("delete from Images");
+            return RedirectToAction("List");
         }
     }
 }
